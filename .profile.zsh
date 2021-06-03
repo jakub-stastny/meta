060d7a47c2d5977a61425d734b09a94c27dd72f9
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-06-03 13:43:23 +0000

    Don't bother starting Emacs session for this one

--- .profile.zsh
+++ .profile.zsh
@@ -1,9 +1,5 @@
 load ~/.zsh/environments/helpers.zsh && save-function-list
-load ~/.zsh/environments/emacs.zsh
 load ~/.zsh/environments/basic.zsh
-
-start-emacs-session
-rename-first-tab
 
 # Custom functions, aliases and hooks.
 in-first-tab && git pull --rebase --autostash &> /dev/null
