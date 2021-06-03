a3ca9847e54f2f34999c7b308514e023f3f5f1e8
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-06-03 15:16:13 +0000

    Move to .env

--- .profile.zsh
+++ .profile.zsh
@@ -2,8 +2,6 @@ load ~/.zsh/environments/helpers.zsh && save-function-list
 load ~/.zsh/environments/basic.zsh
 
 # Custom functions, aliases and hooks.
-BLACKLIST="books.budgeting books.personal-productivity data.blog data.books data.manuals data.spendings experiment flashcards gdrive-cli jakub-stastny meta wasmer-ruby"
-
 typeset -T -U -gx -a BLACKLIST blacklist ' '
 
 blist-add()        { blacklist+=("$@") }
